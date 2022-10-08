3834ffcc5f9c1af136064f65f6d15758c1ab67ed
XuanLuomeng <1079716693@qq.com>
2022-10-09 00:07:50 +0800

    sixth

--- src/main/java/com/cultureIsland/web/filter/CharchaterFliter.java
+++ src/main/java/com/cultureIsland/web/filter/CharchaterFliter.java
@@ -14,22 +14,28 @@ import java.io.IOException;
 public class CharchaterFliter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) {
-
     }
+
     @Override
-    public void doFilter(ServletRequest req, ServletResponse rep, FilterChain filterChain) throws  ServletException, IOException {
+    public void doFilter(ServletRequest req, ServletResponse rep, FilterChain filterChain) throws ServletException, IOException {
         //将父接口转为子接口
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) rep;
         //获取请求方法
         String method = request.getMethod();
         //解决post请求中文数据乱码问题
-        if(method.equalsIgnoreCase("post")){
+        if (method.equalsIgnoreCase("post")) {
             request.setCharacterEncoding("utf-8");
         }
+        String uri = request.getRequestURI();
+        response.setCharacterEncoding("utf-8");
         //处理响应乱码
-        response.setContentType("text/html;charset=utf-8");
-        filterChain.doFilter(request,response);
+        if (uri.contains(".css") || uri.contains(".js") || uri.contains(".png")||uri.contains(".jpg")) {
+            filterChain.doFilter(request, response);
+        }else {
+            response.setContentType("text/html;charset=utf-8");
+            filterChain.doFilter(request, response);
+        }
     }
 
     @Override
